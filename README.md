fef44b23085f7d669a10416c53e5c5b8d6ff307f
mehrabk <mehrab.kor@gmail.com>
2021-12-08 01:40:20 +0330

    complated stream aspect(RTMP server)

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Stream App Like twitch.tv
 
+## CRUD with redux
+
 ### part1 -> react-router -> react-router-dom
 
 ### part2 -> OAuth Authentication (Login With Google Account) | gapi
 
 ### part3 -> i love react-hook-form (XX redux-form -> read for undrestanding of all structure of form handlers XX)
+
+### part4 -> RTMP server and streamming aspect application
--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,21 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import flv from "flv.js";
 
 import { fetchStream } from "../../actions";
 
 export default function StreamShow() {
   const { id } = useParams();
+  // if we come in streamshow with link the streamlist is ready
+  // if we refresh this page streamlist(redux store) is empty and first stream is undefined and then useEffect call and fetch data
+  // as fetch dispatched and reducer run this component render again but now we have stream data in redux store
+  // so we must have render condition that stream is ready or not
   const stream = useSelector((state) => state.streams[id]);
   const dispatch = useDispatch();
   const progNav = useNavigate();
+  // in class component this.videoRef = React.createRef()
+  const videoRef = useRef();
 
   useEffect(() => {
     dispatch(fetchStream(id));
-  }, [id]);
+    const player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    player.attachMediaElement(videoRef.current);
+    player.load();
+    // componentWillUnmount(cleanup function)
+    return () => {
+      // destory for not anymore download
+      player.destroy();
+    };
+  }, []);
 
   return (
     <div>
+      <video ref={videoRef} style={{ width: "100%" }} controls={true} />
       <h1>{stream && stream.title}</h1>
     </div>
   );
